28ff2753415f5cc1aefbb01b9d03426085125bbf
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2026-05-09 21:34:20 +0900

    Time: 0 ms (100%), Space: 149.1 MB (64.29%) - LeetHub

--- 0258-add-digits/0258-add-digits.dart
+++ 0258-add-digits/0258-add-digits.dart
@@ -2,14 +2,12 @@ class Solution {
   int addDigits(int num) {
     while (num >= 10) {
         int sum = 0;
-
         while (num > 0) {
-            sum += num % 10; // 8 // 3 // 1 // 2
-            num ~/= 10; // 3 // 0 // 1 // 0
+            sum += num % 10;
+            num ~/= 10;
         }
-
-        num = sum; // 2
-    } 
+        num = sum;
+    }
     return num;
   }
 }
