fa8eee914e26b6c3a7047df59c53ba63b599b621
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2026-08-03 23:18:59 +0900

    Time: 47 ms (50.38%), Space: 19.2 MB (69.48%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,31 +1,19 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        formatted = f"{x}"
-        reversed = formatted[::-1]
-        result = 0
-        is_negative = False
+        reversed = f"{x}"[::-1]
 
         left_range = -1*2**31
         right_range = 2**31-1
 
         if reversed[-1] == "-":
-            is_negative = True
-            reversed = reversed[:-1] + ""
-            # result = 0 - int(reversed)
-        if reversed[0] == "0":
-            formatted = formatted.replace("0", '', 1)
-            # result = int(reversed)
+            reversed = "-" + reversed[:-1]
+        if reversed[0] == "0" and len(reversed) != 1:
+            reversed = reversed.replace("0", '', 1)
             
-
-        if is_negative == True:
-            result = 0 - int(reversed)
-        else:
-            result = int(reversed)
-            
-        if result < left_range or result > right_range:
+        if int(reversed) < left_range or int(reversed) > right_range:
             return 0
         else:
-            return result
+            return int(reversed)
         
         
         
