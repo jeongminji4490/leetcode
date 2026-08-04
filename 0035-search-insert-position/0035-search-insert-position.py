ee2b590f191ef792082b02ad8a594bcb9f4dce44
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2026-08-04 21:43:13 +0900

    Time: 0 ms (100%), Space: 19.8 MB (81.44%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,21 +1,8 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        result = 0
-
-        min_val = min(nums)
-        max_val = max(nums)
-
-        if target < min_val:
-            return 0
-        if target > max_val:
-            return len(nums)
-
         for i in range(len(nums)):
-            if nums[i] > target:
-                result = i
-                break
-            if nums[i] == target:
-                result = i
-                break
+            if nums[i] >= target:
+                return i
         
-        return result
+        return len(nums)
+            
