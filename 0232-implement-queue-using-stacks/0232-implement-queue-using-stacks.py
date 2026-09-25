578bdbbc536e6f58650326fb684c6be3bf046fe5
Minji Jeong <62979330+jeongminji4490@users.noreply.github.com>
2026-09-25 15:35:33 +0900

    Time: 0 ms (100%), Space: 19.4 MB (88.04%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -0,0 +1,57 @@
+# from collections import deque
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+        self.reversed = []
+        self.size = 0
+
+    # [1, 2] [1, 2]
+    # [1, 2, 3] [3, 2, 1]
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.size += 1
+
+    # [1, 2, 3] -> pop should return 1
+    # reversed [3, 2, 1] -> pop returns 1 -> [3, 2]
+    def pop(self) -> int:
+        # if self.empty():
+        #     return -1
+        # front = self.stack1[0]
+        # del self.stack1[0]
+        # replaced = self.stack1[:-1]
+        # self.stack1.popleft()
+        # LIFO pop은 지키면서 앞에서 pop을 어떻게?
+
+        # else:
+        # popped = self.stack.pop()
+        if self.empty():
+            return -1
+        else:
+            if not self.reversed:
+                for i in range(self.size):
+                    self.reversed.append(self.stack.pop())
+                # popped = self.reversed.pop()
+            self.size -= 1
+        return self.reversed.pop()
+
+    def peek(self) -> int:
+        if self.empty():
+            return -1
+        else:
+            if not self.reversed:
+                for i in range(self.size):
+                    self.reversed.append(self.stack.pop())
+            return self.reversed[-1]
+
+    def empty(self) -> bool:
+        return True if not self.stack and not self.reversed else False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
